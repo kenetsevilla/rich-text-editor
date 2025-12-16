2a0eb80a2ce61534013a41c7cbd281a2dfbff795
Kenet Sevilla <sevillakenet104@gmail.com>
2025-12-16 10:52:58 +0800

    feat: add MainPage layout and fix root element type error

---
+++ client/src/MainPage.tsx
@@ -0,0 +1,18 @@
+import './MainPage.css'
+import Header from './components/header.tsx'
+
+function MainPage() {
+
+  return (
+    <>
+        <div className="mainpage-canvas">
+            <div className="mainpage-container">
+                <Header />
+                <p>Lorem ipsum</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default MainPage
---
+++ client/src/components/header.tsx
@@ -0,0 +1,16 @@
+import './header.css'
+
+function Header() {
+
+  return (
+    <>
+        <div className="header-canvas">
+            <div className="header-container">
+                <p>Header</p>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
